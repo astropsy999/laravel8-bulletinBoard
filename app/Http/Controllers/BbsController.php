ab206f53fabc3f7caaecb56511d657caf884d410
astropsy999 <bareust@gmail.com>
2024-05-09 18:29:33 +0300

    adding templates

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -9,22 +9,28 @@ class BbsController extends Controller
 {
     public function index()
     {
-        $bbs = Bb::latest()->get();
-        $s = "Объявления\r\n\r\n";
-        foreach ($bbs as $bb) {
-            $s .= $bb->title . "\r\n";
-            $s .= $bb->price . " руб.\r\n";
-            $s .= "\r\n";
-        }
-        return response($s)->header('Content-Type', 'text/plain');
+        $context = [
+            'bbs' => Bb::latest()->get()
+        ];
+
+        return view('index', $context);
+        // $bbs = Bb::latest()->get();
+        // $s = "Объявления\r\n\r\n";
+        // foreach ($bbs as $bb) {
+        //     $s .= $bb->title . "\r\n";
+        //     $s .= $bb->price . " руб.\r\n";
+        //     $s .= "\r\n";
+        // }
+        // return response($s)->header('Content-Type', 'text/plain');
     }
 
-    public function detail($bb)
+    public function detail(Bb $bb)
     {
-        $bb = Bb::find($bb);
-        $s = $bb->title . "\r\n\r\n";
-        $s .= $bb->content . "\r\n\r\n";
-        $s .= $bb->price . " руб.\r\n";
-        return response($s)->header('Content-Type', 'text/plain');
+        // $bb = Bb::find($bb);
+        // $s = $bb->title . "\r\n\r\n";
+        // $s .= $bb->content . "\r\n";
+        // $s .= $bb->price . " руб.\r\n";
+        // return response($s)->header('Content-Type', 'text/plain');
+        return view('detail', ['bb' => $bb]);
     }
 }
