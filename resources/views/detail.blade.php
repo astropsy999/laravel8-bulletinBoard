a09126fd0531d72be3bd7ac092e30e15439aa746
astropsy999 <bareust@gmail.com>
2024-05-09 18:52:18 +0300

    Именованные маршруты

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,18 +1,16 @@
 <html>
+@extends('layouts.base')
 
-<head>
-    <meta charset="UTF-8">
-    <title>{{ $bb->title }} :: Объявления</title> ...
-</head>
+@section('title') {{ $bb->title }}
 
-<body>
-    <div class="container">
-        <h1 class="my-3 text-center">Объявления</h1>
-        <h2>{{ $bb->title }}</h2>
-        <p>{{ $bb->content }}</p>
-        <p>{{ $bb->price }} руб.</p>
-        <p><a href="/">На перечень объявлений</a></p>
-    </div>
-</body>
+@section('main')
+
+
+<h2>{{ $bb->title }}</h2>
+<p>{{ $bb->content }}</p>
+<p>{{ $bb->price }} руб.</p>
+<p><a href="{{ route('index') }}">На перечень объявлений</a></p>
+
+@endsection('main')
 
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,41 +1,33 @@
 <!DOCTYPE html>
-<html>
+@extends('layouts.base')
 
-<head>
-    <meta charset="UTF-8">
-    <title>Объявления</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
+@section('title') Главная
 
-<body>
-    <div class="container">
-        <h1 class="my-3 text-center">Объявления</h1>
-        @if (count($bbs) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Товар</th>
-                    <th>Цена, руб.</th>
-                    <th>&nbsp;</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($bbs as $bb)
-                <tr>
-                    <td>
-                        <h3>{{ $bb->title }}</h3>
-                    </td>
-                    <td>{{ $bb->price }}</td>
-                    <td>
-                        <a href="/{{$bb->id}}/">Подробнее...</a>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        @endif
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
+@section('main')
+@if(count ($bbs) > 0)
 
-</html>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Товар</th>
+            <th>Цена, руб.</th>
+            <th>&nbsp;</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($bbs as $bb)
+        <tr>
+            <td>
+                <h3>{{ $bb->title }}</h3>
+            </td>
+            <td>{{ $bb->price }}</td>
+            <td>
+                <a href="{{ route('detail', ['bb' => $bb->id]) }}">Подробнее...</a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endif
+
+@endsection('main')
