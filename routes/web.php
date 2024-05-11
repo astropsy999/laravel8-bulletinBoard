73dbe7010109075b7f15da712b7fb7ad4a423798
astropsy999 <bareust@gmail.com>
2024-05-11 08:09:09 +0300

    authentification implemented

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\BbsController;
+use \Illuminate\Support\Facades\Auth;
 
 // Route::get('/', function () {
 //     return view('welcome');
 
 Route::get('/', [BbsController::class, 'index'])->name('index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
